003bd242634b4e6a13cfe7a24b6ae2ee87217c30
Vedant Andhale <90826179+VedantAndhale@users.noreply.github.com>
2023-09-12 18:47:30 +0530

    Update script.js

--- HCI/prac3/script.js
+++ HCI/prac3/script.js
@@ -1,18 +1,21 @@
 // script.js
+
+// Select all tab buttons and tab content items
 const buttons = document.querySelectorAll('.tab-buttons button');
 const tabItems = document.querySelectorAll('.tab-item');
 
+// Add a click event listener to each tab button
 buttons.forEach((button, index) => {
     button.addEventListener('click', () => {
-        // Remove the "active" class from all buttons and tab items
+        // Remove the "active" class from all buttons and tab content items
         buttons.forEach((btn) => btn.classList.remove('active'));
         tabItems.forEach((item) => item.classList.remove('active'));
 
-        // Add the "active" class to the clicked button and corresponding tab item
+        // Add the "active" class to the clicked button and its corresponding tab content item
         button.classList.add('active');
         tabItems[index].classList.add('active');
     });
 });
 
-// Initially, show the text tab as highlighted
+// Initially, show the Text tab as highlighted
 buttons[0].click();
